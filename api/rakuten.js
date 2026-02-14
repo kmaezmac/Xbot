a47d5b409e7ab15dabe2584f701bdd3dd5adbbdf
Kai Moly <k.maezmac@gmail.com>
2026-02-14 23:32:09 +0900

    アフィid削除

--- api/rakuten.js
+++ api/rakuten.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     var age = args[Math.floor(Math.random() * args.length)];
     var random = Math.floor(Math.random() * 34) + 1;
     var requestUrl = "https://openapi.rakuten.co.jp/ichibaranking/api/IchibaItem/Ranking/20220601?applicationId=" + process.env.RAKUTEN_APP_ID
-        + "&age=" + age + "&sex=1&carrier=0&page=" + random + "&affiliateId=" + process.env.RAKUTEN_AFFILIATE_ID + "&accessKey=" + process.env.RAKUTEN_APP_ACCESS_KEY
+        + "&age=" + age + "&sex=1&carrier=0&page=" + random + "&accessKey=" + process.env.RAKUTEN_APP_ACCESS_KEY
     try {
         const response = await axios.get(requestUrl,
             {
